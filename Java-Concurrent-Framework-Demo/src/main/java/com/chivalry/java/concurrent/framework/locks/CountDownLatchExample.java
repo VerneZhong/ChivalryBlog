bf0ea1c6fda04e815e81e51a85c16b2cbeb67f8e
Mr.zxb <zxb@sengled.com>
2022-03-28 11:14:11 +0800

    CountDownLatch示例

--- Java-Concurrent-Framework-Demo/src/main/java/com/chivalry/java/concurrent/framework/locks/CountDownLatchExample.java
+++ Java-Concurrent-Framework-Demo/src/main/java/com/chivalry/java/concurrent/framework/locks/CountDownLatchExample.java
@@ -1,42 +1,43 @@
 package com.chivalry.java.concurrent.framework.locks;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 /**
- * {@link java.util.concurrent.Semaphore} example class
+ * {@link java.util.concurrent.CountDownLatch} example class
  *
  * @author Mr.zxb
- * @date 2022-03-28 10:32
+ * @date 2022-03-28 11:07
  */
-public class SemaphoreExample {
-
+public class CountDownLatchExample {
     public static final int THREAD_COUNT = 500;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // 创建200个线程数的线程池
         ExecutorService executorService = Executors.newFixedThreadPool(200);
 
         // 设置20个许可证的信号量
-        Semaphore semaphore = new Semaphore(20);
+        CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
 
         // 请求500次
         for (int i = 0; i < THREAD_COUNT; i++) {
             int num = i;
             executorService.execute(() -> {
                 try {
-                    // 获取许可证
-                    semaphore.acquire();
                     // 模拟业务请求
                     test(num);
-                    // 释放许可证
-                    semaphore.release();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                } finally {
+                    // 释放资源
+                    countDownLatch.countDown();
                 }
             });
         }
+        // 等待所有任务完成
+        countDownLatch.await();
         // 关闭线程池
         executorService.shutdown();
         System.out.println("all task finished");
@@ -47,5 +48,4 @@ public class SemaphoreExample {
         System.out.printf("[%s] thread count: %d\n", Thread.currentThread().getName(), num);
         Thread.sleep(1000);
     }
-
 }
