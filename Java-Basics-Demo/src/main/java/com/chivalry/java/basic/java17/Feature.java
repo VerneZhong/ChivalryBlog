36f58210d33ab3b9f62be91c70c07bac7eacf1c0
VerneZhong <verne.zhong@gmail.com>
2023-08-20 15:45:20 +0900

    java17 其他特性

--- Java-Basics-Demo/src/main/java/com/chivalry/java/basic/java17/Feature.java
+++ Java-Basics-Demo/src/main/java/com/chivalry/java/basic/java17/Feature.java
@@ -1,5 +1,11 @@
 package com.chivalry.java.basic.java17;
 
+import java.text.NumberFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.stream.Stream;
+
 /**
  * new feature in Java17 example
  *
@@ -18,6 +24,16 @@ public class Feature {
         System.out.println(feature.switchExpression(Fruit.APPLE));
 
         feature.testRecord();
+
+        feature.instanceofTest(Fruit.APPLE);
+
+//        feature.helpfulNPE(null);
+
+        feature.testDateFormat();
+
+        feature.testNumberFormat();
+
+        feature.testStreamToList();
     }
 
     /**
@@ -65,12 +81,44 @@ public class Feature {
         Person p1 = new Person("小黑", 18, "东京都");
         Person p2 = new Person("小白", 28, "大阪府");
 
-        record PersonRecord(String name, int age) {}
+        record PersonRecord(String name, int age) {
+        }
 
         PersonRecord pr1 = new PersonRecord(p1.getName(), p1.getAge());
         PersonRecord pr2 = new PersonRecord(p2.getName(), p2.getAge());
 
         System.out.println(pr1.name);
         System.out.println(pr2.age);
+    }
+
+    public void instanceofTest(Object o) {
+        if (o instanceof Fruit fruit && "APPLE".equals(fruit.name())) {
+            System.out.println(fruit.name());
+        }
+    }
+
+    public void helpfulNPE(Fruit f) {
+        System.out.println(f.name());
+    }
+
+    public void testDateFormat() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("B");
+        System.out.println(dtf.format(LocalTime.of(8, 0)));
+        System.out.println(dtf.format(LocalTime.of(13, 0)));
+        System.out.println(dtf.format(LocalTime.of(20, 0)));
+        System.out.println(dtf.format(LocalTime.of(23, 0)));
+        System.out.println(dtf.format(LocalTime.of(0, 0)));
+        System.out.println(dtf.format(LocalTime.now()));
+    }
+
+    public void testNumberFormat() {
+        NumberFormat numberFormat = NumberFormat.getCompactNumberInstance(Locale.UK, NumberFormat.Style.SHORT);
+        System.out.println(numberFormat.format(10000));
+        System.out.println(numberFormat.format(1000000));
+        System.out.println(numberFormat.format(1000000000));
+    }
+
+    public void testStreamToList() {
+        Stream.of(1, 2, 3, 4, 5, 6).toList().forEach(System.out::println);
     }
 }
