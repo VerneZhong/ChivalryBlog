9b9832ebca6a25f5bb85b1e61733d3861e3e863e
VerneZhong <verne.zhong@gmail.com>
2023-10-16 21:29:20 +0900

    LeetCode算法练习

--- Algorithm-Demo/src/main/java/com/chivalry/algorithm/solution/Solution.java
+++ Algorithm-Demo/src/main/java/com/chivalry/algorithm/solution/Solution.java
@@ -658,4 +658,36 @@ public class Solution {
         }
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
+
+    /**
+     * Majority Element
+     * Given an array nums of size n, return the majority element.
+     *
+     * The majority element is the element that appears more than ⌊n / 2⌋ times.
+     * You may assume that the majority element always exists in the array.
+     * Example 1:
+     * Input: nums = [3,2,3]
+     * Output: 3
+     *
+     * Example 2:
+     * Input: nums = [2,2,1,1,1,2,2]
+     * Output: 2
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int i = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                i = num;
+                count = 1;
+            } else if (i == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return i;
+    }
 }
