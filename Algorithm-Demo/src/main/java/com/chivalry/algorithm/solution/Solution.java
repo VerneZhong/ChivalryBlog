de9beb2c95db667ed7a4f5dc9762df96ec06e88d
VerneZhong <verne.zhong@gmail.com>
2024-06-10 22:23:48 +0900

    Leetcode之两数之和

--- Algorithm-Demo/src/main/java/com/chivalry/algorithm/solution/Solution.java
+++ Algorithm-Demo/src/main/java/com/chivalry/algorithm/solution/Solution.java
@@ -1,7 +1,6 @@
 package com.chivalry.algorithm.solution;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Mr.zxb
@@ -23,8 +22,12 @@ public class Solution {
 //        String s = "   fly me   to   the moon  ";
 //        System.out.println(solution.lengthOfLastWord(s));
 
-        String a = "1010", b = "1011";
-        System.out.println(solution.addBinary(a, b));
+//        String a = "1010", b = "1011";
+//        System.out.println(solution.addBinary(a, b));
+
+        int[] nums = {2,7,11,15};
+        int[] twoSum = solution.twoSum(nums, 9);
+        System.out.println(Arrays.toString(twoSum));
     }
 
     /**
@@ -689,5 +692,39 @@ public class Solution {
             }
         }
         return i;
+    }
+
+    /**
+     * 1. Two Sum
+     * Given an array of integers nums and an integer target,
+     * return indices of the two numbers such that they add up to target.
+     *
+     * You may assume that each input would have exactly one solution,
+     * and you may not use the same element twice.
+     *
+     * You can return the answer in any order.
+     *
+     * Example 1:
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     *
+     * Example 2:
+     * Input: nums = [3,2,4], target = 6
+     * Output: [1,2]
+     *
+     * Example 3:
+     * Input: nums = [3,3], target = 6
+     * Output: [0,1]
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+        }
+        return new int[0];
     }
 }
