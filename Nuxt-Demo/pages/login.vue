0e49bc68fb8565213cf515b0ec0044d0292e5e17
VerneZhong <verne.zhong@gmail.com>
2025-03-24 20:21:29 +0900

    优化登陆错误显示

--- Nuxt-Demo/pages/login.vue
+++ Nuxt-Demo/pages/login.vue
@@ -16,6 +16,7 @@ const login = async () => {
       username: username.value,
       password: password.value
     })
+    console.log(response)
     // 返回数据包含一个 `token` 或 `status` 字段
     if (response.data.status === 'success') {
       toast.success("登录成功！🎉", { timeout: 2000 })
@@ -28,7 +29,12 @@ const login = async () => {
     }
   } catch (error) {
     // 处理请求错误（如网络问题、后端错误等）
-    toast.error("网络错误，请稍后再试！", { timeout: 2000 })
+    console.log(error)
+    if  (error.response.status === 401) {
+      toast.error("用户名或密码错误！🎉", { timeout: 2000 })
+    } else {
+      toast.error("网络错误，请稍后再试！", { timeout: 2000 })
+    }
   }
 }
 </script>
